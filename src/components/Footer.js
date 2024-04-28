d6a7eaa634f4f56513c90457c5a43f9ad46d124a
Ashwini Mhaske <97236860+ashwinimhaske2003@users.noreply.github.com>
2024-04-29 00:42:17 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/gulshancodes/redux-shopping-cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                       
+                    </a>
+
+                    <p>
+                        Created by | &nbsp;
+                        <a
+                            href="https://github.com/ashwinimhaske2003"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Ashwini Mhaske
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
